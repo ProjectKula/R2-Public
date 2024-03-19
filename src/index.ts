726ea9aeb2002a87ae4b25d3582c7156b1be442b
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-03-19 23:05:25 +0530

    Re-add class A ops

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 interface Env {
-  MY_BUCKET: R2Bucket
+  MY_BUCKET: R2Bucket;
+  AUTH_KEY_SECRET: string;
 }
 
 function objectNotFound(objectName: string): Response {
@@ -11,12 +12,25 @@ function objectNotFound(objectName: string): Response {
   })
 }
 
+function authorizeRequest(request: Request, env: Env) {
+  switch (request.method) {
+    case 'PUT':
+    case 'POST':
+    case 'DELETE':
+      return request.headers.get('X-Auth-Key') === env.AUTH_KEY_SECRET;
+    default:
+      return false;
+  }
+}
+
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
     const url = new URL(request.url)
     const objectName = url.pathname.slice(1)
 
     console.log(`${request.method} object ${objectName}: ${request.url}`)
+
+    authorizeRequest(request, env);
 
     if (request.method == 'GET') {
       const object = await env.MY_BUCKET.get(objectName)
@@ -32,10 +46,20 @@ export default {
       return new Response(object.body, {
         headers
       })
+    } else if (request.method == 'PUT' || request.method == 'POST') {
+      await env.MY_BUCKET.put(objectName, request.body);
+      return new Response(`R2 object "${objectName}" created`, {
+        status: 201
+      })
+    } else if (request.method == 'DELETE') {
+      await env.MY_BUCKET.delete(objectName);
+      return new Response(`R2 object "${objectName}" deleted`, {
+        status: 204
+      })
     }
 
     return new Response(`Unsupported method`, {
-      status: 400
+      status: 405
     })
   }
 }
