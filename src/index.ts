002ee9c98bf4080404f96d3df131b871344035c1
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-03-19 22:41:37 +0530

    Only allow GET

--- src/index.ts
+++ src/index.ts
@@ -18,52 +18,8 @@ export default {
 
     console.log(`${request.method} object ${objectName}: ${request.url}`)
 
-    if (request.method === 'GET' || request.method === 'HEAD') {
-      if (objectName === '') {
-        if (request.method == 'HEAD') {
-          return new Response(undefined, { status: 400 })
-        }
-
-        const options: R2ListOptions = {
-          prefix: url.searchParams.get('prefix') ?? undefined,
-          delimiter: url.searchParams.get('delimiter') ?? undefined,
-          cursor: url.searchParams.get('cursor') ?? undefined,
-          include: ['customMetadata', 'httpMetadata'],
-        }
-        console.log(JSON.stringify(options))
-
-        const listing = await env.MY_BUCKET.list(options)
-        return new Response(JSON.stringify(listing), {
-          headers: {
-            'content-type': 'application/json; charset=UTF-8',
-          }
-        })
-      }
-
-      if (request.method === 'GET') {
-        const object = await env.MY_BUCKET.get(objectName, {
-          range: request.headers,
-          onlyIf: request.headers,
-        })
-
-        if (object === null) {
-          return objectNotFound(objectName)
-        }
-
-        const headers = new Headers()
-        object.writeHttpMetadata(headers)
-        headers.set('etag', object.httpEtag)
-        if (object.range) {
-          headers.set("content-range", `bytes ${object.range.offset}-${object.range.end ?? object.size - 1}/${object.size}`)
-        }
-        const status = object.body ? (request.headers.get("range") !== null ? 206 : 200) : 304
-        return new Response(object.body, {
-          headers,
-          status
-        })
-      }
-
-      const object = await env.MY_BUCKET.head(objectName)
+    if (request.method == 'GET') {
+      const object = await env.MY_BUCKET.get(objectName)
 
       if (object === null) {
         return objectNotFound(objectName)
@@ -72,23 +28,10 @@ export default {
       const headers = new Headers()
       object.writeHttpMetadata(headers)
       headers.set('etag', object.httpEtag)
-      return new Response(null, {
-        headers,
+
+      return new Response(object.body, {
+        headers
       })
-    }
-    if (request.method === 'PUT' || request.method == 'POST') {
-      const object = await env.MY_BUCKET.put(objectName, request.body, {
-        httpMetadata: request.headers,
-      })
-      return new Response(null, {
-        headers: {
-          'etag': object.httpEtag,
-        }
-      })
-    }
-    if (request.method === 'DELETE') {
-      await env.MY_BUCKET.delete(url.pathname.slice(1))
-      return new Response()
     }
 
     return new Response(`Unsupported method`, {
