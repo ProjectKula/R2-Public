9c1c22b63afb9c48c461315a9b46372e69f3ee85
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-06-14 18:30:46 +0530

    Begin split

---
+++ src/auth.ts
@@ -0,0 +1,28 @@
+import { Request } from '@cloudflare/workers-types';
+import { Env } from './index';
+
+async function authorizeFromBackend(request: Request, env: Env): Promise<boolean> {
+	let jwt = request.headers.get("Authorization");
+	if (!jwt) {
+		return false;
+	}
+	let authUrl = env.AUTH_JWT_URL;
+	let resp = await fetch(authUrl, {
+		headers: {
+			"Authorization": jwt,
+		},
+	});
+
+	return resp.ok;
+}
+
+export async function authorizeRequest(request: Request, env: Env) {
+	switch (request.method) {
+		case 'PUT':
+		case 'POST':
+		case 'DELETE':
+			return request.headers.get('X-Auth-Key') === env.AUTH_KEY_SECRET || await authorizeFromBackend(request, env);
+		default:
+			return true;
+	}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,57 @@
-interface Env {
-  MY_BUCKET: R2Bucket;
-  AUTH_KEY_SECRET: string;
+import { authorizeRequest } from './auth';
+import { Request } from '@cloudflare/workers-types';
+
+export interface Env {
+	MY_BUCKET: R2Bucket;
+	AUTH_KEY_SECRET: string;
+	AUTH_JWT_URL: string;
 }
 
 function objectNotFound(objectName: string): Response {
-  return new Response(`<html><body>R2 object "<b>${objectName}</b>" not found</body></html>`, {
-    status: 404,
-    headers: {
-      'content-type': 'text/html; charset=UTF-8'
-    }
-  })
-}
-
-function authorizeRequest(request: Request, env: Env) {
-  switch (request.method) {
-    case 'PUT':
-    case 'POST':
-    case 'DELETE':
-      return request.headers.get('X-Auth-Key') === env.AUTH_KEY_SECRET;
-    default:
-      return false;
-  }
+	return new Response(`<html lang="en"><body>R2 object "<b>${objectName}</b>" not found</body></html>`, {
+		status: 404,
+		headers: {
+			'content-type': 'text/html; charset=UTF-8',
+		},
+	});
 }
 
 export default {
-  async fetch(request: Request, env: Env): Promise<Response> {
-    const url = new URL(request.url)
-    const objectName = url.pathname.slice(1)
+	async fetch(request: Request, env: Env): Promise<Response> {
+		const url = new URL(request.url);
+		const objectName = url.pathname.slice(1);
 
-    console.log(`${request.method} object ${objectName}: ${request.url}`)
+		console.log(`${request.method} object ${objectName}: ${request.url}`);
 
-    authorizeRequest(request, env);
+		if (!await authorizeRequest(request, env)) {
+			return new Response(`Unauthorized`, {
+				status: 401,
+			});
+		}
 
-    if (request.method == 'GET') {
-      const object = await env.MY_BUCKET.get(objectName)
+		if (request.method == 'GET') {
+			const object = await env.MY_BUCKET.get(objectName);
 
-      if (object === null) {
-        return objectNotFound(objectName)
-      }
+			if (object === null) {
+				return objectNotFound(objectName);
+			}
 
-      const headers = new Headers()
-      object.writeHttpMetadata(headers)
-      headers.set('etag', object.httpEtag)
+			const headers = new Headers();
+			object.writeHttpMetadata(headers);
+			headers.set('etag', object.httpEtag);
 
-      return new Response(object.body, {
-        headers
-      })
-    } else if (request.method == 'PUT' || request.method == 'POST') {
-      await env.MY_BUCKET.put(objectName, request.body);
-      return new Response(`R2 object "${objectName}" created`, {
-        status: 201
-      })
-    } else if (request.method == 'DELETE') {
-      await env.MY_BUCKET.delete(objectName);
-      return new Response(`R2 object "${objectName}" deleted`, {
-        status: 204
-      })
-    }
+			return new Response(object.body, {
+				headers,
+			});
+		} else if (request.method == 'PUT' || request.method == 'POST') {
+			await env.MY_BUCKET.put(objectName, request.body);
+			return new Response(`R2 object "${objectName}" created`, {
+				status: 201,
+			});
+		}
 
-    return new Response(`Unsupported method`, {
-      status: 405
-    })
-  }
-}
+		return new Response(`Unsupported method`, {
+			status: 405,
+		});
+	},
+};
